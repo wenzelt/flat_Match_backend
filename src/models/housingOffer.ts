82bb503b145cd84b4fe02b37bea5680567b98494
Tom Wenzel <tom.wenzel@tum.de>
2021-07-23 16:21:28 +0200

    fixed another typo

--- src/models/housingOffer.ts
+++ src/models/housingOffer.ts
@@ -45,7 +45,7 @@ const housingOfferSchema = new mongoose.Schema({
 	},
 	description: {
 		type: String,
-		requried: true
+        required: true
 	},
 	roomSize: {
 		type: Number,
@@ -57,7 +57,7 @@ const housingOfferSchema = new mongoose.Schema({
 	},
 	title: {
 		type: String,
-		requried: true
+        required: true
 	},
 	ageRange: {
 		minAge: {
